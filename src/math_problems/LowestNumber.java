a8ce70079b0f11fc4675a8275067a9bf239e635e
ozlorden <osamanight@gmail.com>
2022-04-10 03:49:57 -0400

    Lowest Number

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -10,6 +10,14 @@ public class LowestNumber {
 
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
+        int min = array[0];
+        //Loop through the array
+        for (int i = 0; i < array.length; i++) {
+            //Compare elements of array with min
+            if(array[i] <min)
+                min = array[i];
+        }
+        System.out.println("Smallest element present in given array: " + min);
 
 
     }
